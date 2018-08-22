51343824f7de1f8c7791c8d1f70ccfa0b5861e04
alan higashi <alanhigashi@gmail.com>
2018-08-22 09:29:47 -0700

    add to homepage and create custom archive

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying case studies
+ * The template for displaying a single case study.
  *
  * @package WordPress
  * @subpackage Accelerate_Theme
@@ -8,18 +8,57 @@
  */
 
 get_header(); ?>
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+	
+<?php while ( have_posts() ) : the_post(); 
+    $services = get_field('services');
+    $client = get_field('client');
+    $link = get_field('site_link');
+	$image_1 = get_field('image_1');
+	$image_2 = get_field('image_2');
+	$image_3 = get_field('image_3');  
+	$size = "full";
+	?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-            <?php while ( have_posts() ) : the_post(); ?>
-            
-            <h1>This is my case studies page</h1>
-            
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><?php the_title(); ?></h2>
+			<h5><?php echo $services; ?></h5>
+			<h6>Client: <?php echo $client; ?></h6>
+
+			<?php the_content(); ?>
+
+			<p><strong><a href="php echo $link; ?>">Site Link</a></strong></p>
+		</aside>
+	
+
+	<div class="case-study-images">
+		<?php if($image_1) { 
+			echo wp_get_attachment_image( $image_1, $size );
+		} ?>
+
+		<?php if($image_2) { 
+			echo wp_get_attachment_image( $image_2, $size );
+		} ?>
+
+		<?php if($image_3) {
+			echo wp_get_attachment_image( $image_3, $size );
+		} ?>
+	</div>
+	</article>
+		<?php endwhile; ?> 
+	
+	</div><!-- .main-content -->
 
 		
-	</div><!-- #primary -->
+</div><!-- #primary -->
 
+<nav id="navigation" class="container">
+	<div class="left"><a href="<?php echo site_url('/case-studies/') 
+		?>">&larr; <span>Back to Work</span></a></div>
+</nav>
+
+            
+    	
 <?php get_footer(); ?>
